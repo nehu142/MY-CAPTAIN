727e7e57d6df21e26c4e764dafc9f4e658662c21
nehu142 <67621852+nehu142@users.noreply.github.com>
2020-07-30 12:37:47 +0530

    Create emp.java

---
+++ emp.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee;
+import java.io.*;
+
+
+/**
+ *
+ * @author HP
+ */
+public class employee 
+{
+
+int y;
+
+String name, adr;
+
+BufferedReader br=new BufferedReader(newInputStreamReader(System.in));
+
+void input();
+
+System.out.println("Enter your year of joining");
+
+y=Integer.parseInt(br.readLine());
+
+System.out.println("Enter your Name");
+
+name=br.readLine();
+
+System.out.println("Enter your addresses");
+
+adr=br.readLine();
+
+    private Reader newInputStreamReader(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static class y {
+
+        public y() {
+        }
+    }
+
+}
+
+ Company
+
+{
+import java.io.*;
+
+class
+public static void main()throws IOException
+
+Employees emp=new Employees();
+
+emp.input();
+
+}
+
+
+    
+}
